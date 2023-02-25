07b15619e3d7d01f325e57fcc47d12b702d9616c
dka <dan005bk@gmail.com>
2023-02-25 19:56:31 +0300

    add intent in OrderDetailActivity

--- app/src/main/java/com/test_projects/cafe/OrderDetailActivity.java
+++ app/src/main/java/com/test_projects/cafe/OrderDetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -13,10 +14,31 @@ public class OrderDetailActivity extends AppCompatActivity {
     private static final String EXTRA_DRINK_TYPE = "drinkType";
     private static final String EXTRA_ADDITIVES = "additives";
 
+    private TextView textViewName;
+    private TextView textViewDrink;
+    private TextView textViewDrinkType;
+    private TextView textViewAdditives;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        initViews();
+
+        Intent intent = getIntent();
+        textViewName.setText(intent.getStringExtra(EXTRA_USER_NAME));
+        textViewDrink.setText(intent.getStringExtra(EXTRA_DRINK));
+        textViewDrinkType.setText(intent.getStringExtra(EXTRA_DRINK_TYPE));
+        textViewAdditives.setText(intent.getStringExtra(EXTRA_ADDITIVES));
+    }
+
+    private void initViews(){
+        textViewName = findViewById(R.id.textViewName);
+        textViewDrink = findViewById(R.id.textViewDrink);
+        textViewDrinkType = findViewById(R.id.textViewDrinkType);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
     }
 
     public static Intent newIntent(Context context, String userName, String drink, String drinkType, String additives) {
